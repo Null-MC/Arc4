e49a9499931a0f59c80886c72a68a1966072c58f
NULL511 <null511@outlook.com>
2026-09-16 16:54:00 -0400

    post/render accumulation

--- src/java/main.java
+++ src/java/main.java
@@ -95,14 +95,14 @@ public class main implements ShaderPack {
 
             // TODO: blur
 
-            if (pipeline.settings().getBoolValue("Accumulation")) {
-                stage.compute("Accumulate-Diffuse", "program/deferred/accumulate", "main")
-                    .overrideObject("texDiffuse_read", diffuseFlipper.getReader().name())
-                    .overrideObject("texDiffuse_write", diffuseFlipper.getWriter().name())
-                    .dispatch2D(sizeX_16, sizeY_16);
+            // if (pipeline.settings().getBoolValue("Accumulation")) {
+            //     stage.compute("Accumulate-Diffuse", "program/deferred/accumulate", "main")
+            //         .overrideObject("texDiffuse_read", diffuseFlipper.getReader().name())
+            //         .overrideObject("texDiffuse_write", diffuseFlipper.getWriter().name())
+            //         .dispatch2D(sizeX_16, sizeY_16);
 
-                diffuseFlipper.flip();
-            }
+            //     diffuseFlipper.flip();
+            // }
 
             stage.compute("OpaqueDeferred", "program/deferred/opaque", "main")
                 .overrideObject("texDiffuse_read", diffuseFlipper.getReader().name())
